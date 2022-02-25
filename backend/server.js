a5d14966225a4c9e206cadf0832247543a2ca7d2
Zhen Yi Pan <zharnite@gmail.com>
2022-02-25 15:00:31 -0500

    error handler and error middleware

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const connectDB = require('./config/db')
+const { errorHandler } = require('./middleware/errorMiddleware')
 const port = 8000
 
 connectDB()
@@ -10,6 +11,8 @@ const app = express()
 app.get('/', (req, res) => {
   res.send('unblur')
 })
+
+app.use(errorHandler)
 
 app.listen(port, () => {
   console.log(`Server started on port: ${port}`)
