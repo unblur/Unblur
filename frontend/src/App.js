885a3d5386c693f52c0d515762a89657265b79a9
Zhen Yi Pan <zharnite@gmail.com>
2022-03-04 20:16:01 -0500

    basic unresponsive header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
 
 function App() {
   return (
     <>
       <Router>
         <div className='container'>
+          <Header />
           <div>unblur app</div>
           <Routes>
             <Route path='/' element={null} />
