7fe4ee224cf3ea94c9876c3e6b8b36e109483e51
xvelez08 <54826206+xvelez08@users.noreply.github.com>
2022-05-10 18:52:42 -0400

    Testing deployment with Azure Web App

--- backend/server.js
+++ backend/server.js
@@ -6,14 +6,15 @@ const connectDB = require('./config/db')
 const { errorHandler } = require('./middleware/errorMiddleware')
 const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('./swagger.json')
-const port = process.env.PORT || 8000
+// const port = process.env.PORT || 8000
 
 connectDB()
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('unblur')
+app.get('*', (req, res) => {
+  res.sendFile(path.normalize(__dirname, "../", "frontend", "src", "index.html"))
+  // res.send('unblur')
 })
 
 // CORS
@@ -27,16 +28,21 @@ app.use('/files', express.static(path.join(__dirname, '..', 'uploads')))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/artworks', require('./routes/artworkRoutes'))
 app.use('/api/transactions', require('./routes/transactionRoutes'))
-
+app.use(express.static('../frontend/build'));
 // API docs
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 // Error handler
 app.use(errorHandler)
+
+
+app.set('port', process.env.PORT || 5000);
+console.log("++++++++++++++++" + app.get('port'));
 
 app.listen(port, () => {
   console.log(`Server started on port: ${port}`)
